6c6bf85d03b23bb1dad03c161a5f53faf878e1ee
Raymond Zhong <raykyri@gmail.com>
2012-05-11 05:21:45 -0400

    Working website with webfonts and transitions, no snappy scrolling yet

---
+++ app.js
@@ -0,0 +1,24 @@
+function screen(s) {
+    $('.overlay .phone .screen')
+	.fadeOut(300, function() {
+		      $(this).attr('style', 'background-image: url(' + s + ');');
+		  });
+
+}
+
+$(document).ready(
+	function() {
+
+	    $('[data-spy="scroll"]').each(function () {
+					      var $spy = $(this).scrollspy('refresh')
+					  });
+	    var default_screen = 'screen_push.png';
+	    screen(default_screen);
+
+	    $('.nav')
+		.bind('activate', function(e) {
+			  screen(
+			      $(e.target).find('a').attr('data-screen')
+			      );
+		      });
+	});
