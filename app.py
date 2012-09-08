eea4e3cbf972fb7b78e7209a1e7512ae90483c5f
Dan Kang <dan@dskang.com>
2012-09-07 20:02:02 -0700

    Use gevent

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+from gevent import monkey; monkey.patch_all()
+from gevent.wsgi import WSGIServer
+
 import os, datetime, urlparse, re
+import logging
+logging.basicConfig()
 
 from pymongo import Connection
 from flask import Flask, request, Response, render_template
@@ -20,26 +25,23 @@ def signup():
                 'ip': request.remote_addr,
                 'time': datetime.datetime.utcnow(),
                 }
-        database.signups.insert(signup)
+        app.database.signups.insert(signup)
         return Response(status=201)
     else:
         return Response(status=400)
 
 def connect_to_db():
     """Connect to database"""
-    global database
-
     MONGOLAB_URI = os.environ['MONGOLAB_URI']
     MONGODB_HOST = urlparse.urlparse(MONGOLAB_URI).geturl()
     MONGODB_PORT = urlparse.urlparse(MONGOLAB_URI).port
     DATABASE_NAME = urlparse.urlparse(MONGOLAB_URI).path[1:]
 
     connection = Connection(MONGODB_HOST, MONGODB_PORT)
-    database = connection[DATABASE_NAME]
+    app.database = connection[DATABASE_NAME]
 
 if __name__ == '__main__':
     connect_to_db()
     port = int(os.environ.get('PORT', 5000))
-    if port == 5000:
-        app.debug = True
-    app.run(host='0.0.0.0', port=port)
+    http_server = WSGIServer(('', port), app)
+    http_server.serve_forever()
